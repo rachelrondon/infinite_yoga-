6a923e24c43ee467668ce74e4a14229d8914fb29
Rachel Rondon <rachel.sun.rondon@gmail.com>
2017-03-29 19:12:52 -0400

    load hello world

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+class LandingPage extends Component {
+
+  render(){
+    return(
+        <h1>Hello World!</h1>
+    );
+  }
+}
+export default LandingPage;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-// Set up your application entry point here...
+import React from "react";
+import ReactDom from "react-dom";
+import { Route, Router, browserHistory } from "react-router";
+
+import LandingPage from './components/LandingPage/LandingPage';
+
+ReactDom.render(
+  <Router history={browserHistory}>
+  <Router path='/' component={LandingPage} />
+ </Router>
+
+,document.getElementById("app"));
